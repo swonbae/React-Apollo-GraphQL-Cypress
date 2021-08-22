14bee7d1d75a536f19be1a70ef1c9a8cbd599bad
swonbae <sbae13@my.centennialcollege.ca>
2021-08-21 21:53:45 -0400

    Fixed Cors Error, Added Games board by date

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./NBA20Secondary20Logo.jpg";
+import Games from "./components/Games";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
+
 function App() {
   return (
-    <div className="App">
-      <p className="font-bold text-red-700 text-3xl">Homepage</p>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <p className="font-bold text-red-700 text-3xl">Homepage</p>
+        <Games />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Games.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useEffect, useState } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+function Games() {
+  const ALL_GAMES_QUERY = gql`
+    query AllGamesQuery($date: String!) {
+      allGames(date: $date) {
+        home_team {
+          full_name
+          id
+        }
+        visitor_team {
+          full_name
+          id
+        }
+        home_team_score
+        visitor_team_score
+        status
+        id
+      }
+    }
+  `;
+
+  //   const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  const [date, setDate] = useState(moment().format("2020-01-28"));
+
+  useEffect(() => {
+    if (!date) setDate(moment().format("YYYY-MM-DD"));
+  }, [date]);
+
+  const { loading, error, data } = useQuery(ALL_GAMES_QUERY, {
+    variables: { date },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) console.log(error);
+
+  return (
+    <div className="flex flex-col items-center">
+      <label htmlFor="date">Show games for: </label>
+      <input
+        className="border-2 border-gray-500 p-2"
+        type="date"
+        id="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        pattern="\d{4}-\d{2}-\d{2}"
+      />
+      {data.allGames.map((game) => (
+        <div className="flex flex-col items-center" key={game.id}>
+          <p className="mt-4">
+            {game.home_team.full_name}
+            {game.home_team_score !== 0 && (
+              <Fragment>: {game.home_team_score}</Fragment>
+            )}{" "}
+            vs. {game.visitor_team.full_name}
+            {game.visitor_team_score !== 0 && (
+              <Fragment>: {game.home_team_score}</Fragment>
+            )}
+          </p>
+          <p>{game.status}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Games;
